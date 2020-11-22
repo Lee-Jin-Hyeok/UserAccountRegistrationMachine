244081da73532f9c38252b0ff3d4842758dfd781
Lee-Jin-Hyeok <0816jinjin@gmail.com>
2020-11-23 01:37:59 +0900

    [UPDATE] user_account.xlsx -> example.xlsx

--- src/main/java/com/dsm/xlsxparser/UserAccountRegistrationMachineApplication.java
+++ src/main/java/com/dsm/xlsxparser/UserAccountRegistrationMachineApplication.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 public class UserAccountRegistrationMachineApplication {
     private static final String PATH = "./";                            // 경로
-    private static final String FILE_NAME = "user_account.xlsx";     // 파일명
+    private static final String FILE_NAME = "example.xlsx";     // 파일명
 
     public static void main(String[] args) {
         XLSXParser parser = new XLSXParser(PATH, FILE_NAME);
